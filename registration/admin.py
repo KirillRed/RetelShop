023ebb6d01bddda0415346cd3a2a69340051d20d
Kirill Red <kirillr1740@gmail.com>
2020-12-08 17:27:49 +0200

    Review registered on admin

--- registration/admin.py
+++ registration/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from . import models
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['rating', 'title', 'text', 'target', 'author', 'pk']
+    list_display_links = ['title']
+    search_fields = ['title']
+
 admin.site.register(models.Client)
+
+admin.site.register(models.Review, ReviewAdmin)
+
