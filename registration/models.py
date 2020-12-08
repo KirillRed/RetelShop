6990eca17e6fa9cc8979ce9ef23a26efca4248af
Kirill Red <kirillr1740@gmail.com>
2020-12-08 17:27:35 +0200

    New model Review

--- registration/models.py
+++ registration/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core import validators
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -8,7 +9,19 @@ class Client(models.Model):
     phone = PhoneNumberField(null=True, unique=True)
     email = models.CharField(max_length=200, null=True)
     profile_pic = models.ImageField(default='default_picture.png' ,null=True)
-    data_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    #From 1 star to 5 stars
+    rating = models.PositiveIntegerField(validators=[validators.MaxValueValidator(5)])
+    title = models.CharField(max_length=50, validators=[validators.MinLengthValidator(2)])
+    text = models.CharField(max_length=500, validators=[validators.MinLengthValidator(5)])
+    #Client-recipient
+    target = models.ForeignKey(to=Client, on_delete=models.CASCADE, related_name='+')
+    #Client-author
+    author = models.ForeignKey(to=Client, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+     
