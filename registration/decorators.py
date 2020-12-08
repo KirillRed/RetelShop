5d436cac0c850a5e17cf3cc544255c77faadaed3
Kirill Red <kirillr1740@gmail.com>
2020-12-08 17:50:04 +0200

    Added wraps

--- registration/decorators.py
+++ registration/decorators.py
@@ -1,12 +1,18 @@
 from django.http.request import HttpRequest
 from django.http.response import HttpResponseForbidden
+from django.shortcuts import redirect
+from functools import wraps
 
 def verified_email(view_func):
+    @wraps(view_func)
     def wrapper_func(request: HttpRequest, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('registration:login')
+        print(request.user.groups.all())
         if request.user.groups.all()[0].name == 'verified_email':
             return view_func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden(
                     'You must verify your email to go to this page')
-                    
+
     return wrapper_func
