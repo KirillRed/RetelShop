a3931b9a9a70c607e8d47480116f45f76e65cb17
Kirill Red <kirillr1740@gmail.com>
2020-12-12 10:26:43 +0200

    many changed

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,28 @@
+from registration.models import Client
 from django.contrib.auth.decorators import login_required
 from django.http.request import HttpRequest
-from django.http.response import HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound
+from django.http.response import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound
 from rest_framework.response import Response
 from django.http import JsonResponse
+from django.conf import settings
 from . import serializers
 from django.views.decorators.csrf import csrf_exempt
 from django.core.paginator import Paginator
 from . import models, forms
 from django.shortcuts import redirect
+from django.core.mail import send_mail
+from django.contrib import messages
 
 """This app works with products logic"""
+
+def test_send_email(request):
+    send_mail('Hello',
+    'Test',
+    'htds8891@gmail.com',
+    ['dasel5287@gmail.com'],
+    fail_silently=False)
+
+    return HttpResponse('hello')
 
 def home(request: HttpRequest):
     """Home page with last added products"""
@@ -22,7 +35,11 @@ def home(request: HttpRequest):
     page = paginator.get_page(page_num)
     objects = page.object_list
     objects_serializer = serializers.ProductSerializer(objects, many=True)
-    context = {'objects': objects_serializer.data, 'pages_number': paginator.num_pages}
+    webpush_settings = getattr(settings, 'WEBPUSH_SETTINGS', {})
+    vapid_key = webpush_settings.get('VAPID_PUBLIC_KEY')
+    user = request.user
+    context = {'objects': objects_serializer.data, 'pages_number': paginator.num_pages,
+                'vapid_key': vapid_key}
     return JsonResponse(context, safe=False)
 
 @login_required
@@ -31,15 +48,13 @@ def add_product(request: HttpRequest):
     """Adds product to market"""
     if request.method == 'POST':
         prodform = forms.ProductForm(request.POST)
-        print(request.POST)
-        print(request.data)
         if prodform.is_valid():
             product = models.Product(
                 title = prodform['title'].value(),
                 description = prodform['description'].value(),
                 price = prodform['price'].value(),
                 photo = prodform['photo'].value(),
-                seller = request.user,
+                seller = request.user.client,
                 category = models.Category.objects.get(pk=
                             prodform['category'].value()),
             )
@@ -51,7 +66,6 @@ def add_product(request: HttpRequest):
 @csrf_exempt
 def edit_product(request: HttpRequest):
     """Edits product"""
-    print(request.user)
     pk = request.GET.get('pk', '')
     if pk == '':
         return HttpResponseBadRequest('Oops... Something went wrong!')
@@ -60,7 +74,7 @@ def edit_product(request: HttpRequest):
     except models.Product.DoesNotExist:
         return HttpResponseNotFound('This product doesn\'t exit!')
     if request.method == 'POST':
-        if product.seller != request.user:
+        if product.seller != request.user.client:
             return HttpResponseForbidden('This isn\'t your product! You can\'t edit it!')
         prodform = forms.ProductForm(request.POST, request.FILES, instance=product)
         if prodform.is_valid():
@@ -80,7 +94,7 @@ def delete_product(request: HttpRequest):
     except models.Product.DoesNotExist:
         return HttpResponseNotFound('This product doesn\'t exit!')
     if request.method == 'DELETE':
-        if product.seller != request.user:
+        if product.seller != request.user.client:
             return HttpResponseForbidden('This isn\'t your product! You can\'t delete it!')
         product.delete()
         return redirect('shop:home')
@@ -96,5 +110,64 @@ def product_detail(request: HttpRequest):
     except models.Product.DoesNotExist:
         return HttpResponseNotFound('This product doesn\'t exit!')
     if request.method == 'GET':
-        serializer = serializers.ProductSerializer(product)
+        serializer = serializers.DetailedProductSerializer(product)
+        return JsonResponse(serializer.data, safe=False)
+
+@csrf_exempt
+def by_category(request):
+    category_pk = request.GET.get('category_pk', '')
+    if category_pk == '':
+        return HttpResponseBadRequest('Oops... Something went wrong')
+    try:
+        models.Category.objects.get(pk=category_pk)
+    except models.Category.DoesNotExist:
+        return HttpResponseNotFound('This category doesn\'t exit!')
+    products = models.Product.objects.filter(category=category_pk)
+    paginator = Paginator(products, 5)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+    objects = page.object_list
+    objects_serializer = serializers.ProductSerializer(objects, many=True)
+    context = {'objects': objects_serializer.data, 'pages_number': paginator.num_pages}
+    return JsonResponse(context, safe=False)
+
+
+@login_required
+@csrf_exempt
+def like(request: HttpRequest):
+    if request.method == 'POST':
+        client = request.user.client
+        product_pk = request.GET.get('pk', '')
+        if product_pk == '':
+            return HttpResponseBadRequest('Oops... Something went wrong')
+        try:
+            models.Product.objects.get(pk=product_pk)
+        except models.Product.DoesNotExist:
+            return HttpResponseNotFound('This product does not exit!')
+        product = models.Product.objects.get(pk=product_pk)
+        if product == '':
+            return HttpResponseBadRequest('Oops... Something went wrong!')
+        if client in product.likes.all():
+            product.likes.remove(client)
+            messages.success(request, 'Like was removed succesfully!')
+            serializer = serializers.ProductSerializer(product)
+            return JsonResponse(serializer.data, safe=False)
+        else:
+            product.likes.add(client)
+            messages.success(request, 'Product was liked succesfully!')
+            serializer = serializers.ProductSerializer(product)
+            return JsonResponse(serializer.data, safe=False)
+            
+@login_required
+def your_products(request: HttpRequest):
+    if request.method == 'GET':
+        client = request.user.client
+        products = models.Product.objects.filter(seller=client)
+        if products.count() == 0:
+            return HttpResponse('You haven\'t got any product!')
+        print(products)
+        serializer = serializers.ProductSerializer(products, many=True)
         return JsonResponse(serializer.data, safe=False)
